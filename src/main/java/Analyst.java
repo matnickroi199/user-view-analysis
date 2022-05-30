af214525289536381c68c59f7d828ba082ac3f26
thangpd <thangphamdinh@admicro.vn>
2022-05-30 16:39:32 +0700

    persist data

--- src/main/java/Analyst.java
+++ src/main/java/Analyst.java
@@ -31,7 +31,8 @@ public class Analyst {
 
         SparkSession spark = Spark.getSession("user-view-analysis");
         String pathLog = HDFS + (isPC ? LogProcessor.PC_OUTPUT_PATH : LogProcessor.MB_OUTPUT_PATH) + date;
-        Dataset<Row> df = spark.read().parquet(pathLog);
+        Dataset<Row> df = spark.read().parquet(pathLog).select(split(col("time")," ").getItem(0).as("date"), col("*"));
+        df.persist();
 
         List<Row> resultOV = overview(df);
         db.insertOverview(resultOV, isPC);
@@ -50,39 +51,36 @@ public class Analyst {
             db.insertLocation(resultLocation);
         }
 
+        df.unpersist();
         MySQL.close();
     }
 
     public List<Row> overview(Dataset<Row> df) {
-        return df.select(split(col("time")," ").getItem(0).as("date"), col("guid"))
-                .groupBy("date").agg(count("guid").as("view"), countDistinct("guid").as("user"))
+        return df.groupBy("date").agg(count("guid").as("view"), countDistinct("guid").as("user"))
                 .collectAsList();
     }
 
     public List<Row> browser(Dataset<Row> df) {
-        return df.select(split(col("time")," ").getItem(0).as("date"), col("guid"), col("browser"))
-                .groupBy("date", "browser").agg(count("guid").as("view"), countDistinct("guid").as("user"))
+        return df.groupBy("date", "browser").agg(count("guid").as("view"), countDistinct("guid").as("user"))
                 .collectAsList();
     }
 
     public List<Row> os(Dataset<Row> df) {
-        return df.select(split(col("time")," ").getItem(0).as("date"), col("guid"), col("os"))
-                .groupBy("date", "os").agg(count("guid").as("view"), countDistinct("guid").as("user"))
+        return df.groupBy("date", "os").agg(count("guid").as("view"), countDistinct("guid").as("user"))
                 .collectAsList();
     }
 
     public List<Row> timeframe(Dataset<Row> df) {
-        return df.select(split(col("time")," ").getItem(0).as("date"), substring(col("time"), 12, 2).cast("int").as("hour"), col("guid"))
+        return df.select(col("date"), substring(col("time"), 12, 2).cast("int").as("hour"), col("guid"))
                 .groupBy("date", "hour").agg(count("guid").as("view"), countDistinct("guid").as("user"))
                 .collectAsList();
     }
 
     public List<Row> location(SparkSession spark, String date, Dataset<Row> df) {
         String logMB = HDFS + LogProcessor.MB_OUTPUT_PATH + date;
-        Dataset<Row> pc = df.filter("loc != -1").select("time", "loc", "guid");
-        Dataset<Row> mb = spark.read().parquet(logMB).filter("loc != -1").select("time", "loc", "guid");
-        return pc.union(mb).select(split(col("time")," ").getItem(0).as("date"), col("loc"), col("guid"))
-                .groupBy("date", "loc").agg(count("guid").as("view"), countDistinct("guid").as("user"))
+        Dataset<Row> pc = df.filter("loc != -1").select("date", "loc", "guid");
+        Dataset<Row> mb = spark.read().parquet(logMB).filter("loc != -1").select(split(col("time")," ").getItem(0).as("date"), col("loc"), col("guid"));
+        return pc.union(mb).groupBy("date", "loc").agg(count("guid").as("view"), countDistinct("guid").as("user"))
                 .collectAsList();
     }
 }
